7f1a901d820a0ad9d6d19040f14090f0ec112f2a
Danny Coates <danny.coates@machinify.com>
2024-11-03 09:04:38 -0700

    change the frontend api a bit

--- floor.js
+++ floor.js
@@ -4,7 +4,7 @@ var asFloor = function(obj, floorLevel, yPosition, errorHandler) {
 
     floor.level = floorLevel;
     floor.yPosition = yPosition;
-    floor.buttonStates = {up: "", down: ""};
+    floor.buttons = {up: false, down: false};
 
     // TODO: Ideally the floor should have a facade where tryTrigger is done
     var tryTrigger = function(event, arg1, arg2, arg3, arg4) {
@@ -14,31 +14,31 @@ var asFloor = function(obj, floorLevel, yPosition, errorHandler) {
     };
 
     floor.pressUpButton = function() {
-        var prev = floor.buttonStates.up;
-        floor.buttonStates.up = "activated";
-        if(prev !== floor.buttonStates.up) {
-            tryTrigger("buttonstate_change", floor.buttonStates);
+        var prev = floor.buttons.up;
+        floor.buttons.up = true;
+        if(prev !== floor.buttons.up) {
+            tryTrigger("buttonstate_change", floor.buttons);
             tryTrigger("up_button_pressed", floor);
         }
     };
 
     floor.pressDownButton = function() {
-        var prev = floor.buttonStates.down;
-        floor.buttonStates.down = "activated";
-        if(prev !== floor.buttonStates.down) {
-            tryTrigger("buttonstate_change", floor.buttonStates);
+        var prev = floor.buttons.down;
+        floor.buttons.down = true;
+        if(prev !== floor.buttons.down) {
+            tryTrigger("buttonstate_change", floor.buttons);
             tryTrigger("down_button_pressed", floor);
         }
     };
 
     floor.elevatorAvailable = function(elevator) {
-        if(elevator.goingUpIndicator && floor.buttonStates.up) {
-            floor.buttonStates.up = "";
-            tryTrigger("buttonstate_change", floor.buttonStates);
+        if(elevator.goingUpIndicator && floor.buttons.up) {
+            floor.buttons.up = false;
+            tryTrigger("buttonstate_change", floor.buttons);
         }
-        if(elevator.goingDownIndicator && floor.buttonStates.down) {
-            floor.buttonStates.down = "";
-            tryTrigger("buttonstate_change", floor.buttonStates);
+        if(elevator.goingDownIndicator && floor.buttons.down) {
+            floor.buttons.down = false;
+            tryTrigger("buttonstate_change", floor.buttons);
         }
     };
 
--- interfaces.js
+++ interfaces.js
@@ -1,85 +1,125 @@
-
 // Interface that hides actual elevator object behind a more robust facade,
 // while also exposing relevant events, and providing some helper queue
 // functions that allow programming without async logic.
-var asElevatorInterface = function(obj, elevator, floorCount, errorHandler) {
-    var elevatorInterface = riot.observable(obj);
+var asElevatorInterface = function (obj, elevator, floorCount, errorHandler) {
+  var elevatorInterface = riot.observable(obj);
 
-    elevatorInterface.destinationQueue = [];
+  elevatorInterface.destinationQueue = [];
 
-    var tryTrigger = function(event, arg1, arg2, arg3, arg4) {
-        try {
-            elevatorInterface.trigger(event, arg1, arg2, arg3, arg4);
-        } catch(e) { errorHandler(e); }
-    };
-
-    elevatorInterface.checkDestinationQueue = function() {
-        if(!elevator.isBusy()) {
-            if(elevatorInterface.destinationQueue.length) {
-                elevator.goToFloor(_.first(elevatorInterface.destinationQueue));
-            } else {
-                tryTrigger("idle");
-            }
-        }
-    };
-
-    // TODO: Write tests for this queueing logic
-    elevatorInterface.goToFloor = function(floorNum, forceNow) {
-        floorNum = limitNumber(Number(floorNum), 0, floorCount - 1);
-        // Auto-prevent immediately duplicate destinations
-        if(elevatorInterface.destinationQueue.length) {
-            var adjacentElement = forceNow ? _.first(elevatorInterface.destinationQueue) : _.last(elevatorInterface.destinationQueue);
-            if(epsilonEquals(floorNum, adjacentElement)) {
-                return;
-            }
-        }
-        elevatorInterface.destinationQueue[(forceNow ? "unshift" : "push")](floorNum);
-        elevatorInterface.checkDestinationQueue();
-    };
-
-    elevatorInterface.stop = function() {
-        elevatorInterface.destinationQueue = [];
-        if(!elevator.isBusy()) {
-            elevator.goToFloor(elevator.getExactFutureFloorIfStopped());
-        }
-    };
-
-    elevatorInterface.getFirstPressedFloor = function() { return elevator.getFirstPressedFloor(); }; // Undocumented and deprecated, will be removed
-    elevatorInterface.getPressedFloors = function() { return elevator.getPressedFloors(); };
-    elevatorInterface.currentFloor = function() { return elevator.currentFloor; };
-    elevatorInterface.maxPassengerCount = function() { return elevator.maxUsers; };
-    elevatorInterface.loadFactor = function() { return elevator.getLoadFactor(); };
-    elevatorInterface.destinationDirection = function() {
-      if(elevator.destinationY === elevator.y) { return "stopped"; }
-      return elevator.destinationY > elevator.y ? "down" : "up";
+  var tryTrigger = function (event, arg1, arg2, arg3, arg4) {
+    try {
+      elevatorInterface.trigger(event, arg1, arg2, arg3, arg4);
+    } catch (e) {
+      errorHandler(e);
     }
-    elevatorInterface.goingUpIndicator = createBoolPassthroughFunction(elevatorInterface, elevator, "goingUpIndicator");
-    elevatorInterface.goingDownIndicator = createBoolPassthroughFunction(elevatorInterface, elevator, "goingDownIndicator");
+  };
 
-    elevator.on("stopped", function(position) {
-        if(elevatorInterface.destinationQueue.length && epsilonEquals(_.first(elevatorInterface.destinationQueue), position)) {
-            // Reached the destination, so remove element at front of queue
-            elevatorInterface.destinationQueue = _.rest(elevatorInterface.destinationQueue);
-            if(elevator.isOnAFloor()) {
-                elevator.wait(1, function() {
-                    elevatorInterface.checkDestinationQueue();
-                });
-            } else {
-                elevatorInterface.checkDestinationQueue();
+  elevatorInterface.checkDestinationQueue = function () {
+    if (!elevator.isBusy()) {
+      if (elevatorInterface.destinationQueue.length) {
+        elevator.goToFloor(_.first(elevatorInterface.destinationQueue));
+      } else {
+        tryTrigger("idle");
+      }
+    }
+  };
+
+  // TODO: Write tests for this queueing logic
+  elevatorInterface.goToFloor = function (floorNum) {
+    floorNum = limitNumber(Number(floorNum), 0, floorCount - 1);
+    // Auto-prevent immediately duplicate destinations
+    if (elevatorInterface.destinationQueue.length) {
+      var adjacentElement = _.first(elevatorInterface.destinationQueue)
+      if (epsilonEquals(floorNum, adjacentElement)) {
+        return;
+      }
+    }
+    elevatorInterface.destinationQueue.unshift(floorNum);
+    elevatorInterface.checkDestinationQueue();
+  };
+
+  elevatorInterface.stop = function () {
+    elevatorInterface.destinationQueue = [];
+    if (!elevator.isBusy()) {
+      elevator.goToFloor(elevator.getExactFutureFloorIfStopped());
+    }
+  };
+
+  Object.defineProperties(elevatorInterface, {
+    currentFloor: {
+      get() {
+        return elevator.currentFloor;
+      },
+    },
+    pressedFloors: {
+        get() {
+            return elevator.getPressedFloors();
+        },
+    },
+    destinationFloor: {
+        get() {
+            return elevator.destinationY;
+        },
+    },
+    maxPassengers: {
+        get() {
+            return elevator.maxUsers;
+        },
+    },
+    movingDirection: {
+        get() {
+            if (elevator.destinationY === elevator.y) {
+                return 0;
             }
-        }
-    });
+            return elevator.destinationY > elevator.y ? -1 : 1;
+        },
+    },
+    loadFactor: {
+        get() {
+            return elevator.getLoadFactor();
+        },
+    },
+  });
+  elevatorInterface.setUpLight = createBoolPassthroughFunction(
+    elevatorInterface,
+    elevator,
+    "goingUpIndicator"
+  );
+  elevatorInterface.setDownLight = createBoolPassthroughFunction(
+    elevatorInterface,
+    elevator,
+    "goingDownIndicator"
+  );
 
-    elevator.on("passing_floor", function(floorNum, direction) {
-        tryTrigger("passing_floor", floorNum, direction);
-    });
+  elevator.on("stopped", function (position) {
+    if (
+      elevatorInterface.destinationQueue.length &&
+      epsilonEquals(_.first(elevatorInterface.destinationQueue), position)
+    ) {
+      // Reached the destination, so remove element at front of queue
+      elevatorInterface.destinationQueue = _.rest(
+        elevatorInterface.destinationQueue
+      );
+      if (elevator.isOnAFloor()) {
+        elevator.wait(1, function () {
+          elevatorInterface.checkDestinationQueue();
+        });
+      } else {
+        elevatorInterface.checkDestinationQueue();
+      }
+    }
+  });
 
-    elevator.on("stopped_at_floor", function(floorNum) {
-        tryTrigger("stopped_at_floor", floorNum);
-    });
-    elevator.on("floor_button_pressed", function(floorNum) {
-        tryTrigger("floor_button_pressed", floorNum);
-    });
+  elevator.on("passing_floor", function (floorNum, direction) {
+    tryTrigger("passing_floor", floorNum, direction);
+  });
 
-    return elevatorInterface;
+  elevator.on("stopped_at_floor", function (floorNum) {
+    tryTrigger("stopped_at_floor", floorNum);
+  });
+  elevator.on("floor_button_pressed", function (floorNum) {
+    tryTrigger("floor_button_pressed", floorNum);
+  });
+
+  return elevatorInterface;
 };
